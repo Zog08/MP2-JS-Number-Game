f2c681d88b71b2cf8b8a46d129b5ec334163406d
Zoe Heathcote <zoe08@hotmail.co.uk>
2024-07-16 19:52:43 +0000

    feat:added html and css for random number area

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,23 @@
 // ideas on how to run code:
-//
+// choose difficulty level (easy, medium, hard)
+// start game button, runs game
+// generate a new random number (display an animation while genrating)
+// click the place in the tower where you think it belongs
+// when placed, generate a new random number (no repeat of first number)
+// place that number
+// repeat until all 6 blocks are filled
+// game over if any are placed incorrectly at any time (modal to display message and offer new game / change difficulty)
+// incremement lost game counter
+// win game if all numbers are placed correctly (modal to display message and offer new game / change difficulty)
+// incrememnt win game counter
+
+
+
+
+// generate a random number
+
+let numA = Math.floor(Math.random)*10
+
+
+
+console.log(numA)
